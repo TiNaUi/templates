0b0bb982636eb793c3a7f27e0f11324593918dd7
CavinHuang <sujinw@qq.com>
2022-09-19 17:17:22 +0800

    feat: 返回首页

--- tina-wallpaper/src/pages/Creator/list.vue
+++ tina-wallpaper/src/pages/Creator/list.vue
@@ -5,11 +5,12 @@
     <CreatorListItem style="margin-top: 30upx;"/>
     <CreatorListItem style="margin-top: 30upx;"/>
   </view>
+  <NavIndexButton />
 </template>
 
 <script lang="ts" setup>
 import CreatorListItem from '@/components/creatorListItem/index.vue';
-
+import NavIndexButton from '@/components/common/nav-index-button.vue'
 defineOptions({
   name: 'CreatorList'
 })
